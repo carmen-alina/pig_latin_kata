3433203bccb8c13281e6aead5a8f98d8436179e7
Carmen-Alina <carmen.panu@gmail.com>
2019-12-03 16:29:38 +0000

    Solution exercise_3

--- exercise_3.rb
+++ exercise_3.rb
@@ -5,6 +5,24 @@
 #      qat   -> atqay
 
 def pig_latin(word)
+  if word.chr == "a" || word.chr == "e" || word.chr == "i" || word.chr == "o" || word.chr == "u"
+    word + "way"
+
+  elsif word.start_with?("qu")
+    array_word = word.chars.to_a
+    array_word << array_word[0]
+    array_word.shift
+    array_word << array_word[0]
+    array_word.shift
+    array_word.join + "ay"
+  else
+    array_word = word.chars.to_a
+    until (array_word[0].to_s == "a") || (array_word[0].to_s == "e") || (array_word[0].to_s == "i") || (array_word[0].to_s == "o") || (array_word[0].to_s == "u") do
+      array_word << array_word[0]
+      array_word.shift
+    end
+    array_word.join + "ay"
+  end
 
 end
 
